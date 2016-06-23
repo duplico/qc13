a42216e95d34c9e2ba358b8e366a3a6d1f5800e3
George Louthan <georgerlouth@nthefourth.com>
2016-06-22 21:54:48 -0500

    Face animations.

--- etc/eyes/generate_eye_anims.py
+++ etc/eyes/generate_eye_anims.py
@@ -19,9 +19,9 @@ eye_trans = {
     16: (1,12),
     17: (1,13),
     18: (1,14),
-    19: (2,13),
-    20: (2,2),
-    21: (2,8)
+    19: (2,11,12,13,14,1),
+    20: (2,1,2,3,4,6),
+    21: (2,6,7,8,9,11)
 }
 
 eye_frames = dict()
@@ -41,43 +41,59 @@ def main():
             if leds == [0]: leds = []
             
             for led in leds:
-                banks[eye_trans[led][0]][eye_trans[led][1]] = 1
+                for channel in eye_trans[led][1:]:
+                    banks[eye_trans[led][0]][channel] = 1
             eye_frames[name] = banks
     
     
     for frame, values in eye_frames.items():
-        frame_uint32 = "0b"
-        for v in values[1]:
+        frame_uint32 = ""
+        for v in values[2]:
             frame_uint32 += (str(v))
         frame_uint32 += "0"
-        for v in values[2]:
+        for v in values[1]:
             frame_uint32 += (str(v))
         frame_uint32 += "0"
         eye_frames_uint32[frame] = frame_uint32
         
     print '#include "../../led_display.h"'
 
+    all_animations = []
+    
     for anim in os.listdir("."):
         if not anim[-3:] == "txt": continue
-        left_anims = []
-        right_anims = []
+        #left_anims = []
+        #right_anims = []
+        anims = []
         lengths = []
         print
         print "// Frames for %s" % anim[:-4]
+        all_animations.append(anim[:-4])
         with open(anim) as f:
             for line in f:
                 if not line: continue
                 line_elements = map(lambda a: a.strip(), line.split(","))
-                left_anims.append(eye_frames_uint32[line_elements[0]])
-                right_anims.append(eye_frames_uint32[line_elements[1]])
+                anims.append("0b%s%s" % (eye_frames_uint32[line_elements[0]], eye_frames_uint32[line_elements[1]]))
+                #left_anims.append(eye_frames_uint32[line_elements[0]])
+                #right_anims.append(eye_frames_uint32[line_elements[1]])
                 lengths.append(line_elements[2])
-        print "uint32_t %s_l_frames[] = {%s};" % (anim[:-4], ", ".join(left_anims))
-        print "uint32_t %s_r_frames[] = {%s};" % (anim[:-4], ", ".join(right_anims))
+        #print "uint32_t %s_l_frames[] = {%s};" % (anim[:-4], ", ".join(left_anims))
+        #print "uint32_t %s_r_frames[] = {%s};" % (anim[:-4], ", ".join(right_anims))
+        print "uint64_t %s_frames[] = {%s};" % (anim[:-4], ", ".join(anims))
         print "uint16_t %s_lengths[] = {%s};" % (anim[:-4], ", ".join(lengths))
         print
         print "// Animation struct for %s" % anim[:-4]
-        print "face_animation_t %s = {%s_l_frames, %s_r_frames, %s_lengths, %s};" % (anim[:-4],anim[:-4],anim[:-4],anim[:-4],len(lengths))
+        print "face_animation_t %s = {%s_frames, %s_lengths, %s};" % (anim[:-4],anim[:-4],anim[:-4],len(lengths))
     
+    print
+    print "// All animations here:"
+    #print "#define FACE_ANIM_NONE 0"
+    print "#define FACE_ANIM_COUNT %d" % len(all_animations)
+    for i in range(len(all_animations)):
+        print "#define FACE_ANIM_%s %d" % (all_animations[i].upper(), i)
+    print
+    all_animations_ptrs = map(lambda a: "&" + a, all_animations)
+    print "face_animation_t *face_all_animations[FACE_ANIM_COUNT] = {%s};" % ", ".join(all_animations_ptrs)
     
 if __name__ == "__main__":
     main()
--- led_display.c
+++ led_display.c
@@ -58,20 +58,64 @@ rgbdelta_t leg_colors_step[8] = {
 
 face_animation_t face_animations[0];
 
-uint16_t face_banks[4];
-uint16_t face_banks_ambient[4];
-uint8_t face_current_animation = FACE_ANIMATION_NONE;
+uint64_t face_curr;
+uint64_t face_ambient = 0xffffffffffffffff;
+uint8_t face_current_animation = FACE_ANIM_NONE;
+uint8_t face_curr_anim_frame = 0;
+uint16_t face_curr_dur = 0;
+uint16_t ambient_brightness = 0x1f00;
 
-void face_set() {
+uint16_t face_frame_dur = 0;
+uint64_t curr_frame = 0;
 
+uint8_t face_state = FACESTATE_AMBIENT;
+
+#define FACE_DUR_STEP 3
+
+void set_face(uint64_t frame) {
+    for (uint8_t i=0; i<64; i++) {
+        if (frame & ((uint64_t) 1 << i)) {
+            tlc_bank_gs[i/16][i%16] = ambient_brightness;
+        } else {
+            tlc_bank_gs[i/16][i%16] = 0x00;
+        }
+    }
 }
 
-void face_start_anim() {
-
+void face_set_ambient(uint8_t amb_index) {
+    face_ambient = cute_lookaround_frames[0];
+    if (face_state == FACESTATE_AMBIENT) {
+        set_face(face_ambient);
+    }
 }
 
-uint32_t curr_l_frame = 0;
-uint32_t curr_r_frame = 0;
+void face_start_anim(uint8_t anim_index) {
+    face_curr_anim_frame = 0;
+    face_current_animation = anim_index;
+    face_curr = face_all_animations[face_current_animation]->frames[0];
+    face_curr_dur = face_all_animations[face_current_animation]->frame_durations[0];
+    face_state = FACESTATE_ANIMATION;
+}
+
+void led_post() {
+    uint64_t chase = 0;
+    set_face(chase);
+    tlc_stage_blank(0);
+    tlc_set_fun();
+    chase = 1;
+    for (uint8_t i=0; i<64; i++) {
+        chase = chase << 1;
+        set_face(chase);
+        delay_millis(7);
+    }
+
+    uint16_t old_ab = ambient_brightness;
+    for (ambient_brightness=0x1000; ambient_brightness;ambient_brightness-=32) {
+        set_face(0xffffffffffffffff);
+    }
+    ambient_brightness = old_ab;
+
+}
 
 void leds_timestep() {
     // Face:
@@ -85,44 +129,36 @@ void leds_timestep() {
     // Tentacles:
     //  Apply our current delta animation timestep.
 
-    // For now:
-
-    curr_l_frame = angry_l_frames[0];
-    curr_r_frame = angry_l_frames[0];
-
-    // bank 0:
-    for (uint8_t channel=0; channel<15; channel++) {
-        if (curr_l_frame & ((uint32_t) 1 << (17+channel)))
-            tlc_bank_gs[0][channel+1] = 0x0f00;
-        else
-            tlc_bank_gs[0][channel+1] = 0;
-    }
-    // bank 1:
-    for (uint8_t channel=0; channel<15; channel++) {
-        if (curr_l_frame & ((uint32_t) 1 << (1+channel)))
-            tlc_bank_gs[1][channel+1] = 0x0f00;
-        else
-            tlc_bank_gs[1][channel+1] = 0;
-    }
-    // bank 2:
-    for (uint8_t channel=0; channel<15; channel++) {
-        if (curr_r_frame & ((uint32_t) 1 << (17+channel)))
-            tlc_bank_gs[2][channel+1] = 0x0f00;
-        else
-            tlc_bank_gs[2][channel+1] = 0;
-    }
-    // bank 3:
-    for (uint8_t channel=0; channel<15; channel++) {
-        if (curr_r_frame & ((uint32_t) 1 << (1+channel)))
-            tlc_bank_gs[3][channel+1] = 0x0f00;
-        else
-            tlc_bank_gs[3][channel+1] = 0;
-    }
-
-    // For now:
-    for (uint8_t bank=4; bank<6; bank++) {
-        for (uint8_t channel=0; channel<12; channel++) {
-            tlc_bank_gs[bank][channel+4] = 0x0f00;
+    if (face_state == FACESTATE_ANIMATION) {
+        if (face_curr_dur < FACE_DUR_STEP) { // Time for next frame?
+            face_curr_anim_frame++;
+            if (face_curr_anim_frame == face_all_animations[face_current_animation]->len) { // done?
+                face_state = FACESTATE_AMBIENT;
+                face_curr = face_ambient;
+                // done
+            } else {
+                face_curr = face_all_animations[face_current_animation]->frames[face_curr_anim_frame];
+                face_curr_dur = face_all_animations[face_current_animation]->frame_durations[face_curr_anim_frame];
+                set_face(face_curr);
+            }
+        } else {
+            // tick towards next frame.
+            face_curr_dur -= FACE_DUR_STEP;
+            // TODO: Check to see if we need to change anything.
         }
     }
+
+    if (face_state == FACESTATE_AMBIENT) {
+        // TODO: Check to see if we need to change anything.
+        set_face(face_ambient);
+        return;
+    }
+
+//    // For now:
+//    for (uint8_t bank=4; bank<6; bank++) {
+//        for (uint8_t channel=0; channel<12; channel++) {
+//            tlc_bank_gs[bank][channel+4] = 0x0f00;
+//        }
+//    }
+
 }
