8348005202b5c2b25b118bbb65d6889ff64cbd15
George Louthan <191906+duplico@users.noreply.github.com>
2016-07-11 09:33:38 -0500

    Quick documentation.

--- badge.h
+++ badge.h
@@ -8,15 +8,16 @@
 #ifndef BADGE_H_
 #define BADGE_H_
 
-void initial_animations();
-void time_loop();
-void start_button_clicked();
-void select_button_clicked();
-void radio_received(qcpayload *payload);
-void radio_transmit_done();
-void leg_anim_done(uint8_t tentacle_anim_id);
-void second();
-void mate_start(uint8_t badge_id);
-void mate_end(uint8_t badge_id);
+void initial_animations(); // Called when it's time to start the first ambients
+void time_loop(); // Called every time_loop_interval
+void start_button_clicked(); // The start button was pressed and released
+void select_button_clicked(); // The select button was pressed and released
+void radio_received(qcpayload *payload); // Received a radio message
+void radio_transmit_done(); // Finished transmitting a radio message
+void leg_anim_done(uint8_t tentacle_anim_id); // Leg animation finished
+void face_animation_done(); // Eye animation finished
+void second(); // Called once per second (ish)
+void mate_start(uint8_t badge_id); // Called when badges mated (o_hai)
+void mate_end(uint8_t badge_id); // Called when badges unmated
 
 #endif /* BADGE_H_ */
