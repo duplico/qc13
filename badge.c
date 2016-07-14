3d022c909f4769ccf6e49f34b63d85a4e2b8a4fd
George Louthan <georgerlouth@nthefourth.com>
2016-07-13 22:09:30 -0500

    Develop payloads and basics of protocols. #41, #39, #34.

--- badge.c
+++ badge.c
@@ -30,7 +30,7 @@ const qc13conf default_conf = {
         0
 };
 
-qcpayload in_payload, out_payload;
+rfbcpayload in_payload, out_payload;
 
 uint8_t being_inked = 0;
 uint8_t mated = 0;
@@ -95,7 +95,7 @@ uint8_t face_anim_no = 0;
 void start_button_clicked() {
     if (being_inked) return; // nope!
     out_payload.ink_id = my_conf.camo_id;
-    out_payload.ink_type = LEG_INK_INDEX;
+    out_payload.flags |= RFBC_INK;
     rfm75_tx();
 
     // The testing code to cycle through faces:
@@ -104,6 +104,8 @@ void start_button_clicked() {
 }
 
 void select_button_clicked() {
+    if (being_inked) return; // nope!
+
     if ((my_conf.camo_id+1) % LEG_ANIM_COUNT == my_conf.camo_id) {
         // not allowed to change camo
     } else {
@@ -121,19 +123,16 @@ void leg_anim_done(uint8_t tentacle_anim_id) {
 
 void ink_received(uint8_t ink_id, uint8_t ink_type, uint8_t from_addr) {
     being_inked = 1;
-    face_start_anim(3);
     tentacle_start_anim(ink_id, ink_type, 3, 0);
 }
 
-void radio_received(qcpayload *payload) {
+void radio_received(rfbcpayload *payload) {
     if (!being_inked && payload->ink_id != LEG_ANIM_NONE) { // it's an ink!
-        ink_received(payload->ink_id, payload->ink_type, payload->from_addr);
+        ink_received(payload->ink_id, ((payload->flags & RFBC_DINK) ? 2 : 1), payload->from_addr);
     }
 }
 
 void radio_transmit_done() {
-    face_start_anim(4);
-    tentacle_start_anim(my_conf.camo_id, LEG_DOUBLEINK_INDEX, 0, 0);
 }
 
 uint64_t mate_old_ambient = 0b1000010000100000111111111111111010000100001000001111111111111110;
