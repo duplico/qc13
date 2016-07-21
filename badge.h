2c92d3fb691b7d675c279cbf71fb06707ad84423
George Louthan <georgerlouth@nthefourth.com>
2016-07-21 14:33:38 -0500

    Add comment.

--- badge.h
+++ badge.h
@@ -15,7 +15,7 @@ void radio_beacon_interval(); // Called when it's time for the radio to beacon.
 void new_badge_seen(uint8_t deferred); // Called when we've seen a new badge
 void new_badge_mated(); // Called when we've mated to a new badge.
 void start_button_clicked(); // The start button was pressed and released
-void start_button_longpressed();
+void start_button_longpressed(); // Start button press & hold (fired before release)
 void select_button_clicked(); // The select button was pressed and released
 void radio_broadcast_received(rfbcpayload *payload); // Received a radio message
 void radio_transmit_done(); // Finished transmitting a radio message
