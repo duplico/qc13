b2a841824d937c8645fa9076962e34438bdc6553
George Louthan <191906+duplico@users.noreply.github.com>
2016-07-21 21:20:26 -0500

    Refactor!

--- metrics.h
+++ metrics.h
@@ -15,3 +15,5 @@ uint8_t is_uber(uint8_t badge_id);
 uint8_t is_handler(uint8_t badge_id);
 uint8_t is_donor(uint8_t badge_id);
 uint8_t is_gilder(uint8_t badge_id);
+uint8_t is_camo_avail(uint8_t camo_id);
+void unlock_camo(uint8_t camo_id);
