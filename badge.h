950df829d31ae1e77265031e67165bd63f7b068c
George Louthan <191906+duplico@users.noreply.github.com>
2016-07-26 12:03:05 -0500

    Long-press select to show ink, resolving #113.

--- badge.h
+++ badge.h
@@ -16,8 +16,10 @@ void new_badge_seen(uint8_t deferred); // Called when we've seen a new badge
 void new_badge_mated(); // Called when we've mated to a new badge.
 void start_button_clicked(); // The start button was pressed and released
 void start_button_longpressed(); // Start button press & hold (fired before release)
+void select_button_longpressed(); // Select button press & hold (fired before release)
 void hat_change(uint8_t from, uint8_t to); // The hat we're wearing has changed.
 void select_button_clicked(); // The select button was pressed and released
+void radio_ink_received(uint8_t ink_id, uint8_t ink_type, uint8_t from_addr);
 void radio_broadcast_received(rfbcpayload *payload); // Received a radio message
 void radio_transmit_done(); // Finished transmitting a radio message
 void leg_anim_done(uint8_t tentacle_anim_id); // Leg animation finished
