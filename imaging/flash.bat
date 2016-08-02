dbeaf98ddd7634445b925265527f1c815af54fce
George Louthan <georgerlouth@nthefourth.com>
2016-08-01 21:17:14 -0500

    Add images

---
+++ imaging/flash.bat
@@ -0,0 +1,2 @@
+python flash.py %1
+msp430flasher -v -w tmp.txt
---
+++ imaging/flash.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import subprocess
+
+prepend = """@1900
+%02X 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+00 00 00 00 00 00 
+"""
+
+if __name__ == "__main__":
+    ti_txt_out = ""
+    badge_id = int(sys.argv[1])
+    prepend = prepend % badge_id
+    ti_txt_out = prepend
+    for line in open("qc13.txt"):
+        ti_txt_out += line
+    with open('tmp.txt', 'w') as tmp_out:
+        tmp_out.write(ti_txt_out)
